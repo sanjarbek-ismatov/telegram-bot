8ac9e7a9823695b521970c9acd4e6a6257886ce8
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-09-27 21:00:21 +0500

    Working api

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const { Telegraf } = require("telegraf");
+const commandParts = require("telegraf-command-parts");
+const Axios = require("axios").default;
 require("dotenv").config();
 const bot = new Telegraf(process.env.TOKEN);
+bot.use(commandParts());
+async function fetchWeather(city, ctx) {
+  await Axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=46a5f1f15caefb45e9b1f9c2d3687e57`
+  )
+    .then((data) => {
+      ctx.reply(data.data.main.temp);
+    })
+    .catch((err) => ctx.reply("Afsus Shahar topilmadi: " + err.code));
+}
 bot.start((ctx) => ctx.reply("Salom"));
-bot.on("text", (ctx) => {
-  ctx.reply("Siz yozgan text: " + ctx.message.text);
+bot.command("/get", (ctx) => {
+  fetchWeather(ctx.state.command.args, ctx);
 });
-bot.on("photo", (ctx) => {
-  ctx.reply(ctx.message.photo);
-});
-bot.command("hello", (ctx) => ctx.reply("Salom zor"));
 bot.launch();
