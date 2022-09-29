08a8f0ef30c954d7b1a712a806f2254ade9c8ab9
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-09-29 21:12:39 +0500

    updated

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ async function fetchWeather(city, ctx) {
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=46a5f1f15caefb45e9b1f9c2d3687e57`
   )
     .then((data) => {
-      ctx.reply("Bugun ob-havo: " + data.data.main.temp + " C");
+      ctx.reply(
+        "🌦 Bugun " + city + "da ob-havo: " + data.data.main.temp + " C"
+      );
     })
     .catch((err) => ctx.reply("Afsus Shahar topilmadi: " + err.code));
 }
-bot.start((ctx) => ctx.reply("Salom"));
+bot.start((ctx) =>
+  ctx.reply(
+    "Salom, Ob havoni bilish uchun /get Tashkent kabi /get City deb kiriting. Xato bo'lsa @Sanjarbek_Ismatov"
+  )
+);
 bot.command("/get", (ctx) => {
   fetchWeather(ctx.state.command.args, ctx);
 });
